0d96212a6032b5490eb6673694fd7e68b655d535
Scott Robertson <scottrobertson97@gmail.com>
2025-12-02 00:48:34 -0500

    2025 day 1 part 2

--- src/2025/1.2.ts
+++ src/2025/1.2.ts
@@ -3,12 +3,13 @@ export default function run(input: string): number {
   let sum = 0
   input.split('\r\n').forEach(line => {
     const direction = line.substring(0, 1) === 'L' ? -1 : 1
-    const amount = Number(line.substring(1))
-    dial += direction * amount
-    while (dial >= 100 || dial < 0) {
-      sum++
-      if (dial >= 100) dial -= 100
-      if (dial < 0) dial += 100
+    let amount = Number(line.substring(1))
+    while (amount > 0) {
+      dial += direction
+      if (dial === -1) dial = 99
+      if (dial === 100) dial = 0
+      if (dial === 0) sum += 1
+      amount -= 1
     }
   })
   return sum
