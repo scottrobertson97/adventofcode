d925cd6d64a975fff5347b3cbcbfec6dd5862a72
Scott Robertson <scottrobertson97@gmail.com>
2025-12-04 08:03:37 -0500

    day 3 part 2

--- src/2025/3.2.ts
+++ src/2025/3.2.ts
@@ -1,8 +1,8 @@
-function getFirstBiggest(line: string): number[] {
-  let index = 0
-  let tens = Number(line[index])
+function getFirstBiggest(line: string, start: number, place: number): number[] {
+  let index = start
+  let tens = Number(line[start])
 
-  for (let i = 1; i < line.length; i++) {
+  for (let i = start+1; i < line.length - (place-1); i++) {
     if (Number(line[i]) > tens) {
       tens = Number(line[i])
       index = i
@@ -13,18 +13,17 @@ function getFirstBiggest(line: string): number[] {
 }
 
 export default function run(input: string): number {
+  const numLength = 12
   let sum = 0
   input.split('\r\n').forEach(line => {
     let lastIndex = 0
     const arr = []
-    for (let p = 2; p > 0; p--) {
-      const [num, index] = getFirstBiggest(
-        line.substring(lastIndex, line.length - lastIndex - p)
-      )
-      arr.push(num.toString())
-      lastIndex = lastIndex + index + 1
+    for (let p = numLength; p > 0; p--) {
+      const [num, index] = getFirstBiggest(line, lastIndex, p)
+      arr.push(num)
+      lastIndex = index + 1
     }
-    sum += Number(arr.join(''))
+    sum += arr.map((n, i) => n * 10 ** (numLength - i - 1)).reduce((a, b) => a + b, 0)
   })
 
   return sum
