3d859fbaf936df4c84307b8807627c88e23884cd
Scott Robertson <scottrobertson97@gmail.com>
2025-12-04 03:37:47 -0500

    day 4

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { solutions } from './index.ts'
 
 const year = 2025
-const day = 1
-const part = 2
+const day = 4
+const part = 1
 
 const input = await Deno.readTextFile(`./input/${year}.${day}.txt`)
 const answer = solutions[year][day][part](input)
---
+++ src/2025/4.1.ts
@@ -0,0 +1,29 @@
+function check8Around(grid: string[][], x: number, y: number): boolean {
+  const directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]
+  ]
+
+  let othersAround = 0
+  for (const [dx, dy] of directions) {
+    if (grid?.[x + dx]?.[y + dy] === '@') othersAround++
+  }
+
+  return othersAround < 4
+}
+
+export default function run(input: string): number {
+  let sum = 0
+  const grid = input.split('\r\n').map(line => line.split(''))
+
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[0].length; y++) {
+      if (grid[x][y] === '@' && check8Around(grid, x, y)) {
+        sum++
+      }
+    }
+  }
+
+  return sum
+}
