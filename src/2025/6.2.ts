45c60bb3e559ed2a64e75d815487eab03dd448d5
Scott Robertson <scottrobertson97@gmail.com>
2025-12-06 10:01:10 -0500

    20025.6.2

--- src/2025/6.2.ts
+++ src/2025/6.2.ts
@@ -1,6 +1,8 @@
 export default function run(input: string): number {
   let sum = 0
+
   const lines = input.split('\r\n').map(l => l.split(''))
+
   const inputs = []
 
   for (let x = 0; x < lines[0].length; x++) {
@@ -24,18 +26,38 @@ export default function run(input: string): number {
       currentGroup.push(line)
     }
   }
+  groups.push(currentGroup)
+  console.log(groups)
 
-  groups.forEach(group => {
+  const formatGroups = groups.map(group => {
     const op = group[0].substring(group[0].length - 1)
     group[0] = group[0].substring(0, group[0].length - 1)
-    const addNotMultiply = op === '+'
-    let subSum = addNotMultiply ? 0 : 1
-    for (let i = 0; i < group.length; i++) {
-      const num = Number(group[i].trim())
-      if (addNotMultiply) subSum += num
-      else subSum *= num
+    const nums = group.map(Number)
+    let sum = op === '+' ? 0 : 1
+    for (const num of nums) {
+      if (op === '+') sum += num
+      else sum *= num
     }
-    sum += subSum
+    return { op, nums, sum }
+  })
+
+  console.log(formatGroups)
+
+  // const formatGroups = groups.forEach(group => {
+  //   const op = group[0].substring(group[0].length - 1)
+  //   group[0] = group[0].substring(0, group[0].length - 1)
+  //   const addNotMultiply = op === '+'
+  //   let subSum = addNotMultiply ? 0 : 1
+  //   for (let i = 0; i < group.length; i++) {
+  //     const num = Number(group[i].trim())
+  //     if (addNotMultiply) subSum += num
+  //     else subSum *= num
+  //   }
+  //   sum += subSum
+  // })
+
+  formatGroups.forEach(g => {
+    sum += g.sum
   })
 
   return sum
